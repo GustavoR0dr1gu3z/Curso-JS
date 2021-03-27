dd5a7fe69cb8a6124806e43b7c54ce4ccf5d57ae
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-26 19:39:19 -0600

    variables JS

---
+++ JS-Basico/variables.js
@@ -0,0 +1,22 @@
+// Palabra reservada para variables: var
+
+var nombre = "Gus";
+
+// Dos estados de una variable
+// 1.- Declarar: Generar con la palabra var
+var edad;
+
+// 2.- Inicializar la variable
+edad = 21;
+
+edad
+
+var elementos = ["computadora", "Celular"];
+
+var persona = {
+    nombre: "Gustavo",
+    edad: 30
+};
+
+persona;
+
