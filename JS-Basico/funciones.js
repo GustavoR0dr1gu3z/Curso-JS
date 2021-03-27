a0620d81a6cb95d728dc887d71305ae4ddbd37b7
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-26 21:07:57 -0600

    funciones JS

---
+++ JS-Basico/funciones.js
@@ -0,0 +1,15 @@
+// Dos tipos de funciones
+
+// 1.- FUnciones declarativas
+function miFuncion(){    
+    return 3;
+}
+miFuncion();
+
+
+// 2.- Funciones de expresión o anónimas
+var miFuncionV = function(a, b){
+    return a + b;
+}
+
+miFuncionV(3,2);
