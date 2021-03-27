51a3db40d1a74bbd25b69eb6bee0e02ca97678b4
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-26 21:55:05 -0600

    truthy and falsy JS

---
+++ JS-Basico/Truthy_Falsy.js
@@ -0,0 +1,28 @@
+// Saber que valores son falsos o verdaderos por defecto
+
+// False
+Boolean(null);
+
+// False
+Boolean(0);
+
+// True
+Boolean(1);
+
+// False
+Boolean(Nan);
+
+// False
+Boolean(undefined);
+
+//True
+Boolean("a");
+
+//True
+Boolean(53);
+
+//True
+Boolean({});
+
+//True
+Boolean([]);
