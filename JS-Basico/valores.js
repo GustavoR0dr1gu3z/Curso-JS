6c513beb731d30c16266cb58c411d6f64ebea09d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-26 19:25:41 -0600

    valores en JS

---
+++ JS-Basico/valores.js
@@ -0,0 +1,19 @@
+// Numero
+40
+
+// Texto
+"Gustavo Calzada"
+
+// Valores Booleanos
+true
+false
+
+// Valores nulos o indefinidos
+null
+undefined
+
+// Valores NO primitivos
+// Array, tipo objeto
+[1,2,3]
+
+{nombre: "Gus"}
