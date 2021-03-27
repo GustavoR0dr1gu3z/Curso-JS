32b05d3fcd1c2cf245a56c126b79c618ffac7ddb
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-26 22:05:15 -0600

    operadores JS

---
+++ JS-Basico/operadores.js
@@ -0,0 +1,65 @@
+// Operadores de operaciones matemáticas
+
+// Suma
+res = 3 + 2;
+
+// Resta
+res = 5 - 4;
+
+// Multiplicación
+res = 8 * 5;
+
+// División
+res = 4 / 2;
+
+// Concatenación: Juntar 2 valores String
+var nom = "Gustavo" + "Calzada";
+
+// Negación
+!false // Sale verdadero
+!true  // Sale negativo
+
+// Asignación: =
+var a = 1;
+
+// Comparar: ==
+3 == "5";
+
+
+// Validando que ambos tipos sean estrictamente iguales
+3 === 3;  //True
+3 === "3"; //False
+
+// Menor o igual
+3 <= 5;
+
+// Mayor o igual
+4 >= 2;
+
+// A es verdad
+aaa && bbb;
+
+// Una de las 2, tiene que ser verdad
+aaa || bbb;
+
+
+var edad = 40;
+
+// Se incrementa en 1
+edad ++;
+
+// Se incrementa en 2
+edad += 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
