205347dd01967116afc058c0c22bc2c440827f6f
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-27 21:25:22 -0600

    switch JS

---
+++ JS-Basico/switch.js
@@ -0,0 +1,23 @@
+var numero = 1;
+
+switch(numero){
+
+    case 1: 
+        console.log("Numero 1");
+        break;
+    
+    case 3:
+        console.log("Numero 3");
+        break;
+
+    case 10:
+        console.log("Numero 10");
+        break;
+
+    case 100:
+        console.log("Numero 100");
+        break;
+
+    default:
+        console.log("No soy nada");
+}
